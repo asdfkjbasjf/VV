775491d91838d568614a33beeecc12bc0cc4d2be
asdfkjbasjf <lordofsheepish@gmail.com>
2025-02-04 20:19:43 +0545

    Update indexxx.js

--- indexxx.js
+++ indexxx.js
@@ -1,4 +1,5 @@
 const no = document.getElementById("no")
+no.addEventListener("click",increase);
 no.addEventListener("mouseover",func);
 no.addEventListener("mouseenter",func);
 function func(){
@@ -9,6 +10,14 @@ function func2(){
     no.textContent="No";
 
 }
+function increase() {
+    const btn = document.getElementById("yes");
+    const currwidth = btn.clientWidth;
+    const currheight = btn.clientHeight;
+    btn.style.width = (currwidth + 20) + "px";
+    btn.style.height = (currheight + 10) + "px";
+} 
+
 
 
 
